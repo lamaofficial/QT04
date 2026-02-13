1f74ef8ce570bb90aaeaffbe96648b64666b967d
lamaofficial <reallearning0004@gmail.com>
2026-02-13 15:01:48 +0800

    clangd, .vscode update

--- build.ps1
+++ build.ps1
@@ -2,6 +2,6 @@ cd build
 Remove-Item * -Recurse -Force
 $env:PATH = "D:\Qt\Tools\mingw1310_64\bin;" + $env:PATH
 cmake -G "MinGW Makefiles" -DCMAKE_PREFIX_PATH="D:\Qt\6.10.2\mingw_64" ..
-cmake --build .
+cmake --build . --config Release
 cd bin
 D:\Qt\6.10.2\mingw_64\bin\windeployqt.exe QT04_mingw.exe
--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
-    
-    MainWindow window;
-    window.show();
-    
-    return app.exec();
+int main(int argc, char *argv[]) {
+	QApplication app(argc, argv);
+
+	printf("Hello, World!\n");	
+	
+
+	int c;
+	
+	c = 2;
+
+	c = 3;
+
+	printf("A%d", c);
+
+	MainWindow window;
+	window.show();
+
+	return app.exec();
 }
